be6c1b1885ce1af5967c29385bb0062878ef1157
Aleksandr <apodgainiy@gmail.com>
2022-01-29 07:02:52 +0300

    2

---
+++ updater.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+lastCommit=$(git log -n 1 --oneline | awk '{print $1}')
+echo $lastCommit
+git pull
+newCommit=$(git log -n 1 --oneline | awk '{print $1}')
+echo $newCommit
+if ["$lastCommit" != "$newCommit"]; then
+echo "запускаем обновление"
+else
+echo "обновление не требуется"
+fi
+
